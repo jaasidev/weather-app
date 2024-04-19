c17f1e751da26f06098bf612faabdffd4a6bdf3c
Jaasiel Beltran <jaasiel0303@gmail.com>
2024-04-19 17:08:02 -0400

    detalles a imagenes  y importnado algunos iconos

--- src/const/const.js
+++ src/const/const.js
@@ -1,3 +1,3 @@
 const apiKey = 'b5bdee64d2724f14a8311007241604'
 
-export const apiCall = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=London&aqi=no&days=3`
+export const apiCall = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=&aqi=no&days=3`
