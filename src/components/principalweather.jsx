d82f6a7f12488e2a90f3c95b0f424d46d70bb5a1
Jaasiel Beltran <jaasiel0303@gmail.com>
2024-04-19 18:29:17 -0400

    componiendo algunas cards a usar

--- src/components/principalweather.jsx
+++ src/components/principalweather.jsx
@@ -1,7 +1,24 @@
-export function PrincipalWeather () {
+import { Ubicacion } from './icons'
+
+export function PrincipalWeather ({ temperature, city, Tiempo }) {
   return (
-    <div className='flex flex-col justify-center items-center'>
-      <img src='http://github.com/jaasidev/weather-app/blob/main/img/day/113.svg' width='100px' height='100px' />
+    <div className='flex flex-col justify-center items-center w-full h-full'>
+      <Tiempo />
+      <h2>{temperature}</h2>
+      <div className='flex items-center justify-center gap-1'>
+        <Ubicacion />
+        <span>{city}</span>
+      </div>
+    </div>
+  )
+}
+
+export function Card ({ Tiempo, temperature, fecha }) {
+  return (
+    <div className='flex flex-col justify-center items-center w-full h-full'>
+      <span className='self-start'>{fecha}</span>
+      <Tiempo />
+      <h4>{temperature}</h4>
     </div>
   )
 }
