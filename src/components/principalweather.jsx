32511a4dcf31c1e4ca8a4e199a7bbdf4aab8af7f
Jaasiel Beltran <jaasiel0303@gmail.com>
2024-04-19 18:09:41 -0400

    actu del repo

--- src/components/principalweather.jsx
+++ src/components/principalweather.jsx
@@ -1,7 +1,7 @@
 export function PrincipalWeather () {
   return (
     <div className='flex flex-col justify-center items-center'>
-      {}
+      <img src='http://github.com/jaasidev/weather-app/blob/main/img/day/113.svg' width='100px' height='100px' />
     </div>
   )
 }
