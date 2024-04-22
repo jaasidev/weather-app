dc8ca27d0a8f997665319bfbe52b4a3c1f453e2a
Jaasiel Beltran <jaasiel0303@gmail.com>
2024-04-22 11:19:10 -0400

    ajustando la ui con un mook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import { Button } from './components/buttonlocation'
 import { PrincipalWeather } from './components/cardsweather'
+import result from './mooks/response.json'
 
 function App () {
   return (
     <>
-      <PrincipalWeather city='La Habana' code={1003} temperature='12 Grados' />
+      <PrincipalWeather code={result.current.condition.code} temperature={`${result.location.region},${result.location.country}`} isday={result.current.is_day} />
       <Button />
     </>
   )
--- src/components/buttonlocation.jsx
+++ src/components/buttonlocation.jsx
@@ -2,18 +2,8 @@ import { Ubicacion } from './icons'
 
 export function Button () {
   return (
-    <button className='flex justify-center items-center bg-blue-400 rounded-full p-5 absolute r-2'>
+    <button className='flex justify-center items-center bg-blue-400 rounded-full p-5 sm:absolute fixed bottom-4 right-4 sm:right-4 sm:bottom-4'>
       <Ubicacion />
     </button>
-  )
-}
-
-export function Search () {
-  return (
-    <svg class='w-6 h-6 text-gray-800 dark:text-white' aria-hidden='true' xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='currentColor' viewBox='0 0 24 24'>
-      <path d='M10 2a8 8 0 1 0 0 16 8 8 0 0 0 0-16Z' />
-      <path fill-rule='evenodd' d='M21.707 21.707a1 1 0 0 1-1.414 0l-3.5-3.5a1 1 0 0 1 1.414-1.414l3.5 3.5a1 1 0 0 1 0 1.414Z' clip-rule='evenodd' />
-    </svg>
-
   )
 }
