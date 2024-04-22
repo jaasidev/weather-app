d361e6a3ee0b4badcc6b8cf1506f3a70312e4f85
Jaasiel Beltran <jaasiel0303@gmail.com>
2024-04-22 11:18:48 -0400

    estilando un poco

--- src/components/cardsweather.jsx
+++ src/components/cardsweather.jsx
@@ -1,15 +1,17 @@
 import { Ubicacion } from './icons'
 import { Clima } from './weathericons'
 
-export function PrincipalWeather ({ temperature, city, code }) {
+export function PrincipalWeather ({ temperature, city, code, props, isday }) {
   return (
-    <div className='flex flex-col justify-center items-center w-80 h-full w-ma'>
-      <Clima codigo={code} dia />
-      <h2>{temperature}</h2>
-      <div className='flex items-center justify-center gap-1'>
-        <Ubicacion />
-        <span>{city}</span>
+    <article className={props}>
+      <div className='flex flex-col justify-center items-center w-80 h-full w-ma'>
+        <Clima codigo={code} dia={Boolean(isday)} />
+        <h2>{temperature}</h2>
+        <div className='flex items-center justify-center gap-1'>
+          <Ubicacion />
+          <span>{city}</span>
+        </div>
       </div>
-    </div>
+    </article>
   )
 }
