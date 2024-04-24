2f4477186b2e2acdd2c64aff21c3bc409453e20c
Jaasiel Beltran <jaasiel0303@gmail.com>
2024-04-24 10:08:58 -0400

    haciendo cambios en la ui

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import result from './mooks/response.json'
 function App () {
   return (
     <main className='p-0 relative w-full lg:rounded-lg flex flex-col items-center justify-start lg:justify-center gap-2 lg:grid lg:grid-cols-6 lg:grid-rows-3 lg:shadow-md lg:bg-gray-200 lg:place-content-center lg:pr-2 lg:min-h-[750px]'>
-      <Principal />
+      <Principal result={result} />
       <Proximo tiempo={result.forecast.forecastday} />
       <Estadistica result={result} />
       <Button />
--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,14 +1,16 @@
 import { Card } from './card'
 export function Proximo ({ tiempo }) {
-  return (
-    <ul before='Próximos Días:' className='flex items-center lg:col-start-4 lg:col-end-6 justify-between gap-2 py-2 w-full min-w-[336px] lg:row-span-1 lg:h-5/6 lg:self-end lg:before:content-[attr(before)] lg:relative lg:before:absolute lg:before:top-[-20px] lg:before:left-1 lg:before:font-medium lg:before:text-xl'>
-      {tiempo.map((value, index) => {
-        return index === 0
-          ? null
-          : (
-            <Card fecha={index} key={index} temperature={value.day.maxtemp_c} code={value.day.condition.code} props='flex flex-col justify-center items-center w-full h-full w-1/2 bg-white p-3 md:p-5 rounded-md shadow-md h-40 md:p-5 lg:p-7' />
-            )
-      })}
-    </ul>
-  )
+  return tiempo === null || tiempo === undefined
+    ? null
+    : (
+      <ul before='Próximos Días:' className='flex items-center lg:col-start-4 lg:col-end-6 justify-between gap-2 py-2 w-full min-w-[336px] lg:row-span-1 lg:h-5/6 lg:self-end lg:before:content-[attr(before)] lg:relative lg:before:absolute lg:before:top-[-20px] lg:before:left-1 lg:before:font-medium lg:before:text-xl'>
+        {tiempo.map((value, index) => {
+          return index === 0
+            ? null
+            : (
+              <Card fecha={index} key={index} temperature={value.day.maxtemp_c} code={value.day.condition.code} props='flex flex-col justify-center items-center w-full h-full w-1/2 bg-white p-3 md:p-5 rounded-md shadow-md h-40 md:p-5 lg:p-7' />
+              )
+        })}
+      </ul>
+      )
 }
