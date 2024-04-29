ee687d16de92d81af31b2c47857d09bdbaf72ab6
Jaasiel Beltran <jaasiel0303@gmail.com>
2024-04-29 16:34:49 -0400

    cambios en el logo

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App () {
     fetching('auto:ip')
   }, [])
   return (
-    <main className='relative w-full bg-[--primary-100] flex flex-col items-center  lg:rounded-lg lg:justify-center gap-2 sm:grid sm:grid-col-6 sm:grid-rows-6 sm:max-h-[1100px] lg:grid lg:grid-cols-12 lg:grid-rows-12 lg:shadow-md lg:bg-[--bg-200] lg:place-content-center lg:pr-2 lg:max-h-[850px] lg:max-w-[1200px] p-1  pb-2 sm:pb-10 lg:p-0'>
+    <main className='relative w-full bg-[--primary-100] flex flex-col items-center  lg:rounded-lg lg:justify-center gap-2 sm:grid sm:grid-col-6 sm:grid-rows-6 sm:max-h-[1100px] lg:grid lg:grid-cols-12 lg:grid-rows-12 lg:shadow-md lg:bg-[--bg-200] lg:place-content-center lg:pr-2 lg:max-h-[850px] lg:max-w-[1200px] p-1  pb-2 sm:pb-10 lg:p-0 xl'>
       <Principal contenido={result} />
       <Forecast contenido={result} />
       <Estadisticas contenido={result} />
