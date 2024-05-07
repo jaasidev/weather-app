fef91c88d286e3de9f3e12757dcd51c642f6fc97
Jaasiel Beltran <jaasiel0303@gmail.com>
2024-05-07 15:46:05 -0400

    cambios en ui para hacer el no response mobile

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Interfaz } from './components/interfaz'
 
 function App () {
   return (
-    <main className='relative w-full bg-[--primary-100] flex flex-col items-center  lg:rounded-lg lg:justify-center gap-2 sm:grid sm:grid-col-6 sm:grid-rows-6 sm:max-h-[1100px] lg:grid lg:grid-cols-12 lg:grid-rows-12 lg:shadow-md lg:bg-[--bg-200] lg:place-content-center lg:max-h-[850px] lg:max-w-[1200px] p-1  pb-2 sm:pb-10 lg:p-0 xl'>
+    <main className='relative w-full bg-[--primary-100] flex flex-col items-center lg:rounded-lg lg:justify-center gap-2 sm:grid sm:grid-col-6 sm:grid-rows-6 sm:max-h-[1100px] lg:grid lg:grid-cols-12 lg:grid-rows-12 lg:shadow-md lg:bg-[--bg-200] lg:place-content-center lg:max-h-[850px] lg:max-w-[1200px] p-1  pb-2 sm:pb-10 lg:p-0 xl'>
       <Interfaz />
       <Button />
     </main>
--- src/components/noresult.jsx
+++ src/components/noresult.jsx
@@ -5,7 +5,7 @@ export function NoResult () {
   return (
     <div className='flex flex-col items-center justify-center col-start-1 col-end-13 row-start-1 row-end-13 bg-[--bg-100] h-full w-full p-3 min-h-[667px] rounded-lg'>
       <Form />
-      <div className='flex flex-1 flex-col items-center justify-center text-3xl text-[--text-200] '>
+      <div className='flex flex-1 flex-col items-center justify-center text-3xl text-[--text-200] text-center'>
         <CancelIco width='150px' height='150px' />
         <span>No se encuentran resultados</span>
       </div>
