2b98b52851a434be90ce1e573dddce4624238407
Jaasiel Beltran <jaasiel0303@gmail.com>
2024-05-14 07:26:25 -0400

    pequeños cambios de ui en el footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,11 @@
 export function Footer () {
   return (
-    <footer className='flex flex-col items-center justify-center text-xl text-[--text-100]'>
+    <footer className='flex items-center justify-center text-lg text-[--text-200] p-3 flex-col sm:flex-row sm:mt-4 lg:fixed lg:bottom-0'>
       <span>
         Hecho con ☕️ desde Cuba
+      </span>
+      <span className='hidden sm:block'>
+        -
       </span>
       <span>
         Jaasiel Beltran Alvarez
