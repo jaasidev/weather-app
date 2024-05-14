87bc188a30dfdfc8c434ab13d04dfba2d7d3c585
Jaasiel Beltran <jaasiel0303@gmail.com>
2024-05-14 07:17:31 -0400

    agregando un footer a la app

---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+export function Footer () {
+  return (
+    <footer className='flex flex-col items-center justify-center text-xl text-[--text-100]'>
+      <span>
+        Hecho con ☕️ desde Cuba
+      </span>
+      <span>
+        Jaasiel Beltran Alvarez
+      </span>
+    </footer>
+  )
+}
